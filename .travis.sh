266e3806eb56a6d526dedfc5c62683182ffc681d
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-02-19 17:03:53 +0100

    Conditionally remove files from the tests

--- .travis.sh
+++ .travis.sh
@@ -10,11 +10,19 @@ php --version
   curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 )
 
-if "$(hhvm use_hhvm_for_composer.hh)"
+runtime=$(hhvm --php -r "HHVM_VERSION < 40000 ? 'php' : 'hhvm';")
+if [ "$runtime" = "hhvm"]
 then
   hhvm /usr/local/bin/composer install
 else
   composer install
+fi
+
+use_polyfill=$(hhvm --php -r "echo HHVM_VERSION_ID < 42600 ? 'old' : 'new';")
+if [ "$use_polyfill" = "old" ]; then
+  rm src/fb_intercept.hh
+else
+  rm src/fb_intercept.new.hh
 fi
 
 hh_client
--- use_hhvm_for_composer.hh
+++
@@ -1,3 +0,0 @@
-<?hh // partial
-
-echo HHVM_VERSION_ID < 40000 ? 'true' : 'false';
