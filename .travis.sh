c238b9ae219beadc8711cb242e6ee390cbddee94
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-02-19 17:35:08 +0100

    Delete fileswise with references before composer
    Otherwise never hhvm version won't build the autoloader

--- .travis.sh
+++ .travis.sh
@@ -10,18 +10,18 @@ php --version
   curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 )
 
-runtime=$(hhvm --php -r "HHVM_VERSION < 40000 ? 'php' : 'hhvm';")
-if [ "$runtime" = "hhvm"]; then
-  hhvm /usr/local/bin/composer install
-else
-  composer install
-fi
-
 use_polyfill=$(hhvm --php -r "echo HHVM_VERSION_ID < 42600 ? 'old' : 'new';")
 if [ "$use_polyfill" = "old" ]; then
   rm src/fb_intercept.new.hh
 else
   rm src/fb_intercept.hh
+fi
+
+runtime=$(hhvm --php -r "HHVM_VERSION < 40000 ? 'php' : 'hhvm';")
+if [ "$runtime" = "hhvm"]; then
+  hhvm /usr/local/bin/composer install
+else
+  composer install
 fi
 
 hh_client
