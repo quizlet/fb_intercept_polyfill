46f8b11877c86f4619c2a15d10907d4099067238
tyronjung-quizlet <tyron.jung@quizlet.com>
2020-03-19 03:45:30 -0700

    Update Travis CI

--- .travis.sh
+++ .travis.sh
@@ -10,13 +10,6 @@ php --version
   curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 )
 
-use_polyfill=$(hhvm --php -r "echo HHVM_VERSION_ID < 42600 ? 'old' : 'new';")
-if [ "$use_polyfill" = "old" ]; then
-  rm src/fb_intercept.new.hh
-else
-  rm src/fb_intercept.hh
-fi
-
 runtime=$(hhvm --php -r "echo HHVM_VERSION_ID >= 40000 ? 'php' : 'hhvm';")
 if [ "$runtime" = "hhvm" ]; then
   removetestfiles=$(hhvm --php -r "echo HHVM_VERSION_ID < 32800 ? 'yes' : 'no';")
